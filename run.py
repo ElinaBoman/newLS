6cbb1c7e20b2e8f20b22cb895e159c0e7bac5946
ElinaBoman <>
2023-10-26 13:49:11 +0000

    adjusted validation to check that all values are ints.

--- run.py
+++ run.py
@@ -16,7 +16,6 @@ def get_sales_data():
     """
     Get sales figure input from the user
     """
-
     print('Please enter sales data from the last market.')
     print('Data should be six numbers, separeted by commas.')
     print('Example: 10,20,30,40,50,60\n')
@@ -26,6 +25,7 @@ def get_sales_data():
     sales_data = data_str.split(',')
     validate_data(sales_data)
 
+
 def validate_data(values):
     """
     Inside the try, convert all string values into untegers.
@@ -34,9 +34,11 @@ def validate_data(values):
     """
     
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
-        f'Exactly 6 values required, you provided {len(values)}')
+                f'Exactly 6 values required, you provided {len(values)}'
+                )
     except ValueError as e:
             print(f'Invalid data: {e}, please try again.\n')
 
